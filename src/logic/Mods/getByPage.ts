83c075611431a84712f183022c7b442eab3f619a
tomasci <zivgta@gmail.com>
2021-08-13 01:48:58 +0300

    Routes & logic updates, paw tests updates

---
+++ src/logic/Mods/getByPage.ts
@@ -0,0 +1,81 @@
+import express from "express"
+import Response from "../../utils/Response/Response"
+import db from "../../utils/Database/Database"
+import {site_posts} from "../../../prisma/client"
+
+interface Limit {
+	startPoint: number
+	rowsCount: number
+}
+
+const postsPerPage = 2
+
+async function getByPage(req: express.Request, res: express.Response) {
+	let page = Number(req.params.page)
+
+	if (!Boolean(page)) {
+		return Response(res, {
+			error: true,
+			code: 500,
+			message: "Page value must be Number. #202131208164400",
+		})
+	}
+
+	let limit = computeLimit(page)
+	let posts = await getPosts(limit)
+	let pages = await computePagesCount()
+
+	if (posts.length < 1) {
+		return Response(res, {
+			error: true,
+			code: 404,
+			message: "Page not found. #202131208171300",
+		})
+	}
+
+	return Response(
+		res,
+		{
+			error: false,
+		},
+		{
+			posts,
+			pages,
+		}
+	)
+}
+
+function computeLimit(page: number): Limit {
+	// select * from site_posts order by updatedAt desc limit 0, 1;
+
+	return {
+		startPoint: page * postsPerPage - postsPerPage,
+		rowsCount: postsPerPage,
+	}
+}
+
+async function computePagesCount(): Promise<number> {
+	let postsCount = await db.site_posts.count()
+	return Math.ceil(postsCount / postsPerPage)
+}
+
+async function getPosts(limit: Limit): Promise<site_posts[]> {
+	return db.site_posts.findMany({
+		skip: limit.startPoint,
+		take: limit.rowsCount,
+		orderBy: [
+			{
+				updatedAt: "desc",
+			},
+		],
+		include: {
+			site_post_attachments: {
+				include: {
+					mc_addons: true,
+				},
+			},
+		},
+	})
+}
+
+export default getByPage
--- src/routes/Mods.ts
+++ src/routes/Mods.ts
@@ -1,10 +1,18 @@
 import express from "express"
 import getMod from "../logic/Mods/getMod"
+import getByPage from "../logic/Mods/getByPage"
 
 const router = express()
 
 router.get("/get/:id", async (req: express.Request, res: express.Response) => {
 	await getMod(req, res)
 })
+
+router.get(
+	"/page/:page",
+	async (req: express.Request, res: express.Response) => {
+		await getByPage(req, res)
+	}
+)
 
 export const modsRouter = router
